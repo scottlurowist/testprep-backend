3c15d79b157342f903e50c5ca085dd2b36554fe1
scottlurowist <scottlurowist@gmail.com>
2020-10-27 07:12:51 -0400

    A WORKING VERSION OF THE BACKEND SANS TOKENS
    I have a working version of the backend, but requiring tokens is
    currently disabled for rapid development. This will need to be enabled
    before release.
    
    I deleted the example routes.
    
    I changed the database name in db.js
    
    Server.js had the example routes removed and the test routes added.
    
    I created the model test.js and the routes file test._routes.js
    
    I created the postman folder that replaces the curl-scripts folder
    which I deleted.

---
+++ app/models/test.js
@@ -0,0 +1,62 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// test.js
+//
+// This model represents a question for the testprep database.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+const mongoose = require('mongoose');
+
+
+
+
+const testSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  // A test has zero or more questions. Questions have 1 or more choices.
+  // One or more choices may be correct.
+  "questions": [{
+    "question": {
+      "text": {
+        type: String,
+        required: true
+      }, 
+      "type": {
+        type: String,
+        required: true
+      },
+        
+      "choices": [{
+        "choice": {
+          "text": {
+            type: String,
+            required: true
+          },
+          "isAnswer": {
+            type:Boolean,
+            required: true
+          }
+        }
+      }]
+    }
+  }],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: false
+  }
+}, 
+{
+  timestamps: true
+})
+
+
+module.exports = mongoose.model('Test', testSchema);
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
-const userRoutes = require('./app/routes/user_routes')
+const userRoutes = require('./app/routes/user_routes');
+const testRoutes = require('./app/routes/test_routes');
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -61,8 +61,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
-app.use(userRoutes)
+app.use(userRoutes);
+app.use(testRoutes);
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
