cff04c469d047e6e69682b55d450b4e799664b88
Scott Lurowist <scottlurowist@gmail.com>
2022-12-14 19:32:38 -0500

    ADDED PRODUCTION CONN STR TO ATLAS TWEAKED DB.js
    user_routes.js changed "bcrypt" to "bcryptjs"
    
    server.js I added "userUnifiedTopolog: true" since it was recommended
    when I connected to Atlas.
    
    db.js now is using the production connection string but sensitive
    elements are passed as Docker environment variables.

--- config/db.js
+++ config/db.js
@@ -1,20 +1,21 @@
-'use strict'
+"use strict";
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'testprep'
+const mongooseBaseName = "testprep";
 
 // create the mongodb uri for development and test
 const database = {
   development: `mongodb://localhost/${mongooseBaseName}-development`,
-  test: `mongodb://localhost/${mongooseBaseName}-test`
-}
+  test: `mongodb://localhost/${mongooseBaseName}-test`,
+  production: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+};
 
 // Identify if development environment is test or development
 // select DB based on whether a test file was executed before `server.js`
-const localDb = process.env.TESTENV ? database.test : database.development
+const localDb = process.env.TESTENV ? database.test : database.development;
 
 // Environment variable MONGODB_URI will be available in
 // heroku production evironment otherwise use test or development db
-const currentDb = process.env.DB_URI || localDb
+const currentDb = database.production || localDb;
 
-module.exports = currentDb
+module.exports = currentDb;
